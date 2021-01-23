7fdd09f98663a888e930865b3f8f1cea70b2688d
MamadovZaki <mamadovzicx@gmail.com>
2021-01-23 19:50:52 +0200

    [Server Setup]Added boilerplate code

---
+++ app.js
@@ -0,0 +1,19 @@
+/* Create express server */
+const express = require("express");
+/* Create enviroment variables */
+const dotenv = require("dotenv");
+
+// Load config
+dotenv.config({ path: "./config/config.env" });
+
+/* create an instance of express app */
+const app = express();
+
+/* use process.env to access dotenv file */
+const PORT = process.env.PORT || 5000;
+
+/* test the server */
+app.listen(
+  PORT,
+  console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
