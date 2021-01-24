42f128b68d9e02d59ea390a2bde2d9a4bc2f7b66
MamadovZaki <mamadovzicx@gmail.com>
2021-01-24 09:21:33 +0200

    Added authentication middleware
    middleware/ includes auth.js that has 2 functions, both ensure that user can't access dashboard or any
    page inside the website unless they make sure that user is authenticated first.

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ const express = require("express");
 /* Create an express router object */
 const router = express.Router();
 
+const { ensureAuthentication, ensureGuest } = require("../middleware/auth");
+
 // todo: Login/Landing Page -- @route: GET/
-router.get("/", (request, response) => {
+router.get("/", ensureGuest, (request, response) => {
   response.render("login", {
     layout: "login",
   });
 });
 
 //todo: Dashboard -- @route: GET/
-router.get("/dashboard", (request, response) => {
+router.get("/dashboard", ensureAuthentication, (request, response) => {
   response.render("dashboard");
 });
 
